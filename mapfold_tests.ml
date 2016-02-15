2bd93d1183f747bb3320bc525be9bfc3ef0b901d
Elbert Gong <elbertgong@college.harvard.edu>
2016-02-15 12:30:17 -0500

    2nd draft of mapfold tests

--- mapfold_tests.ml
+++ mapfold_tests.ml
@@ -29,6 +29,24 @@ let test () =
     [Some 0.; Some 2.; None; None]);
   assert ((log10s [1.; 1000.]) = [Some 0.; Some 3.]);
 
+  assert ((deoptionalize [Some 1; Some 4; None; Some 5]) = [1; 4; 5]);
+  assert ((deoptionalize [Some true; Some false; None; None]) =
+    [true; false]);
+
+  assert ((some_sum [None; Some 1; Some 4; Some 9]) = 14);
+  assert ((some_sum [Some -2; Some 7; None; Some 8]) = 13);
+
+  assert ((mult_odds [1;3;0;2;-5]) = -15);
+  assert ((mult_odds [2; 0; -4]) = 1);
+
+  assert ((concat [[1; 2; 3]; []; [4]; [5; 6]]) = [1; 2; 3; 4; 5; 6]);
+  assert ((concat [['h'; 'e']; ['l']; ['l'; 'o']) = 
+    ['h'; 'e'; 'l'; 'l'; 'o']);
+
+  assert ((filter_by_year [("Joe",2010);("Bob",2010);("Tom",2013)]
+    2010) = ["Joe";"Bob"]);
+  assert ((filter_by_year [("I", 1);("A", 2);("O", 4)] 3) = [])
+
 ;;
 
 test();;
