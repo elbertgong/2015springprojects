f9c9bcf249775d841c38a09d558a2da12fb8652c
Elbert Gong <elbertgong@college.harvard.edu>
2016-02-15 13:33:37 -0500

    1st draft of expression tests

--- expression_tests.ml
+++ expression_tests.ml
@@ -5,9 +5,21 @@ open ExpressionLibrary ;;
 let test () =
   assert (contains_var (parse "x+3"));
   assert (not (contains_var (parse "2")));
+  assert (not (contains_var (parse "4-3")));
+  assert (contains_var (parse "3^2+~sin(cos(x))"));
+  assert (not (contains_var (parse "ln(4/(8^2)")));
+  assert (contains_var (parse "4^(4*~x"));
 
-  (*  Additional tests go here... *)
-
+  assert (evaluate (parse "x^4 + 3") 2.0 = 19.0);
+  assert ((evaluate (parse "x+3") 8.4) = 11.4);
+  assert ((evaluate (parse "2") 3) = 2.);
+  assert (evaluate (parse "3^2+~sin(cos(x))") 3.14159) > 9.84 &&
+    (evaluate (parse "3^2+~sin(cos(x))") 3.14159) < 9.85 );
+  assert ((evaluate (parse "ln(4/(8^(2/x)))") 10.) > .97 &&
+    (evaluate (parse "ln(4/(8^(2/x)))") 10.) < .98);
+  assert ((evaluate (parse "4^(4*~x") .2) > .32 &&
+    (evaluate (parse "4^(4*~x") .2) < .33);
+    
 ;;
 
 test();;
