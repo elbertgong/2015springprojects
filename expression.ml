9bf2c0e0855f8ff523cf113c4661a159260ba413
Elbert Gong <elbertgong@college.harvard.edu>
2016-02-15 02:06:25 -0500

    first draft of expression

--- expression.ml
+++ expression.ml
@@ -13,7 +13,11 @@ open ExpressionLibrary ;;
  *     Examples : contains_var (parse "x^4") = true
  *                contains_var (parse "4+3") = false *)
 let rec contains_var (e:expression) : bool =
-    failwith "Not implemented"
+    match e with
+    | Var -> true
+    | Binop (a, b, c) -> contains_var b || contains_var c
+    | Unop (a, b) -> contains_var b
+    | Num a -> false
 ;;
 
 
@@ -23,8 +27,23 @@ let rec contains_var (e:expression) : bool =
  *            worry about handling 'divide by zero' errors.
  *  Example : evaluate (parse "x^4 + 3") 2.0 = 19.0 *)
 let rec evaluate (e:expression) (x:float) : float =
-    failwith "Not implemented"
-;;
+    match e with
+    | Var -> x
+    | Num f -> f
+    | Unop (u, e1) ->
+      (match u with
+      | Sin -> sin (evaluate e1)
+      | Cos -> cos (evaluate e1)
+      | Ln -> log (evaluate e1)
+      | Neg -> -(evaluate e1))
+    | Binop (b, e1, e2) ->
+      match b with
+      | Add -> (evaluate e1) +. (evaluate e2)
+      | Sub -> (evaluate e1) -. (evaluate e2)
+      | Mul -> (evaluate e1) *. (evaluate e2)
+      | Div -> (evaluate e1) /. (evaluate e2)
+      | Pow -> (evaluate e1) ** (evaluate e2)
+ ;;
 
 
 (*>* Problem 2.3 *>*)
@@ -33,12 +52,12 @@ let rec evaluate (e:expression) (x:float) : float =
 let rec derivative (e:expression) : expression =
     match e with
     | Num _ -> Num 0.
-    | Var -> failwith "Not implemented"
+    | Var -> 1.
     | Unop (u,e1) ->
         (match u with
-        | Sin -> failwith "Not implemented"
+        | Sin -> Binop(Mul,Unop(Cos, e1),derivative e1)
         | Cos -> Binop(Mul,Unop(Neg,Unop(Sin,e1)),derivative e1)
-        | Ln -> failwith "Not implemented"
+        | Ln -> Binop(Div, derivative e1, e1)
         | Neg -> Unop(Neg,derivative e1))
     | Binop (b,e1,e2) ->
         match b with
@@ -46,11 +65,15 @@ let rec derivative (e:expression) : expression =
         | Sub -> Binop(Sub,derivative e1,derivative e2)
         | Mul -> Binop(Add,Binop(Mul,e1,derivative e2),
                         Binop(Mul,derivative e1,e2))
-        | Div -> failwith "Not implemented"
+        | Div -> Binop(Div, Binop(Sub, Binop(Mul, derivative e1, e2), 
+            Binop(Mul, e1, derivative e2)), Binop(Pow, e2, 2.))
         | Pow ->
-            if failwith "Not implemented"
-            then failwith "Not implemented"
-            else failwith "Not implemented"
+            if contains_var e2
+            then Binop(Mul, Binop(Pow, e1, e2), 
+              Binop(Sum, Binop(Mul, derivative e2, Unop(Ln, e1)),
+              Binop(Div, Binop(Mul, derivative e1, e2), e1)))
+            else Binop(Mul, Binop(Mul, e2, derivative e1), 
+                Binop(Pow, e1, Binop(Sub, h, 1.)))
 ;;
 
 (* A helpful function for testing. See the writeup. *)
@@ -76,7 +99,9 @@ let checkexp strs xval =
 (* See writeup for instructions. *)
 let rec find_zero (e:expression) (g:float) (epsilon:float) (lim:int)
     : float option =
-    failwith "Not implemented"
+    if lim = 0 then None else
+    if abs (evaluate e g) <= epsilon then Some g else
+    find_zero e (g - (evaluate e g /. evaluate (derivate e) g) epsilon (lim - 1)
 ;;
 
 
